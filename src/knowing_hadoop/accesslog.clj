6e7d897e432c5234d8d418f979b86d9dfbd42f1e
Ji Zhang <jizhang@anjuke.com>
2013-05-07 09:11:09 +0800

    use vec

--- src/knowing_hadoop/accesslog.clj
+++ src/knowing_hadoop/accesslog.clj
@@ -32,7 +32,7 @@
     "0"))
 
 (defn parse-log [log]
-  (let [arr (util/split-line log)]
+  (let [arr (vec (util/split-line log))]
     {"request_time" (divide-1k (nth arr 4))
      "upstream_response_time" (divide-1k (nth arr 5))
      "remote_addr" (nth arr 6)
