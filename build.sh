71543a89342ec080971ea36e8cb6c978ae1608f4
Ji ZHANG <jizhang@anjuke.com>
2015-03-14 16:11:19 +0800

    use internal clojure-hadoop

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 git pull
+rm -fr ~/.m2/repository/clojure-hadoop
 lein do clean, compile, uberjar
-scp target/knowing-hadoop-0.1.0-SNAPSHOT-standalone.jar 10.10.6.99:/tmp
+scp target/knowing-hadoop-0.1.0-SNAPSHOT-standalone.jar xapp10-127:/tmp/knowing-hadoop.jar
--- project.clj
+++ project.clj
@@ -3,17 +3,23 @@
   :url "http://knowing.corp.anjuke.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [clj-yaml "0.4.0"]
                  [com.netflix.curator/curator-framework "1.3.3"]
                  [org.clojure/data.json "0.2.2"]
                  [clj-time "0.5.0"]
-                 [clojure-hadoop "1.4.2" :exclusions [org.apache.hadoop/hadoop-core]]
+                 [clojure-hadoop "1.4.4-ANJUKE"
+                  :exclusions [org.apache.hadoop/hadoop-common
+                               org.apache.hadoop/hadoop-client]]
                  [org.clojure/java.jdbc "0.2.3"]
                  [mysql/mysql-connector-java "5.1.24"]
-                 [com.hadoop.gplcompression/hadoop-lzo "0.4.16"]
                  [org.clojure/tools.logging "0.2.6"]]
-  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "1.0.4"]]}}
+  :mirrors {"central" {:name "nexus"
+                        :url "http://10.20.8.31:8081/nexus/content/groups/public"}
+            #"clojars" {:name "nexus"
+                        :url "http://10.20.8.31:8081/nexus/content/groups/public"}}
+  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-common "2.3.0"]
+                                  [org.apache.hadoop/hadoop-client "2.3.0"]]}}
   :plugins [[lein2-eclipse "2.0.0"]]
   :main knowing-hadoop.core
   :aot [knowing-hadoop.core])
